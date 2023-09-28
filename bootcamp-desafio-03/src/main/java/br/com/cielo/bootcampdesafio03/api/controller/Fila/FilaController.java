04785d4415a070731b4714c62280ee4839196fdb
rafael roco <rafae.roco.dev@gmail.com>
2023-09-28 03:11:56 -0300

    desafio 3 100% completo

--- bootcamp-desafio-03/src/main/java/br/com/cielo/bootcampdesafio03/api/controller/Fila/FilaController.java
+++ bootcamp-desafio-03/src/main/java/br/com/cielo/bootcampdesafio03/api/controller/Fila/FilaController.java
@@ -2,23 +2,24 @@ package br.com.cielo.bootcampdesafio03.api.controller.Fila;
 
 import io.awspring.cloud.sqs.annotation.SqsListener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/fila")
+
+@Service
 public class FilaController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FilaController.class);
     public static final String QUEUE = "bootcamp-cielo.fifo";
 
     @SqsListener(QUEUE)
-    @GetMapping
-    public ResponseEntity<Object> firstElementRow(@Payload Object object){
-        return ResponseEntity.ok().body(object);
+    public void listen(@Payload Object message) {
+        LOGGER.info("Received message {} {}", message);
+        System.out.println(message);
     }
-
 
 }
